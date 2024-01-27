f6c6d740f6755acfc1c7e75c43569e924ff0541b
Adam Nash <adam@smr.llc>
2024-01-27 12:38:12 -0500

    refactor cat and human controls into a player base class

--- human/Human.gd
+++ human/Human.gd
@@ -1,126 +1,18 @@
-extends CharacterBody3D
-
-const SPEED = 22.0
-const ROTATION_SPEED = 5.0
-const JUMP_VELOCITY = 10.0
-
-var mouse_sensitivity = 0.15
-
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gravity_vector = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
-
-var pid: int
-var nametag: String = ""
-
-var buffered_sync: BufferedSync
-
-var jump_requested: bool = false
-var x_movement: float = 0.0
-var z_movement: float = 0.0
-var y_rotation: float = 0.0
-
-var last_x_input: float
-var last_z_input: float
-
-var state_key: String
+extends PlayerBase
 
 func _ready():
-	buffered_sync = get_node("../../../BufferedSync")
-	state_key = "%d" % pid
+	super._ready()
 	if multiplayer.get_unique_id() == pid:
-		$Camera3D.make_current()
 		$InitPopup.popup_centered_ratio.call_deferred(0.5)
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-@rpc("any_peer")
-func do_a_jump():
-	if multiplayer.get_remote_sender_id() != pid:
-		return
-	jump_requested = true
-	
-@rpc("any_peer")
-func set_x_movement(amount: float):
-	if multiplayer.get_remote_sender_id() != pid:
-		return
-	x_movement = clampf(amount, -1.0, 1.0)
-	
-@rpc("any_peer")
-func set_z_movement(amount: float):
-	if multiplayer.get_remote_sender_id() != pid:
-		return
-	z_movement = clampf(amount, -1.0, 1.0)
-
-@rpc("any_peer")
-func add_y_rotation(amount: float):
-	if multiplayer.get_remote_sender_id() != pid:
-		return
-	y_rotation += amount
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		add_y_rotation.rpc_id(1, -1 * deg_to_rad(event.relative.x * mouse_sensitivity))
+	super._input(event)
 
 func _unhandled_input(event):
-	if multiplayer.get_unique_id() != pid:
-		return
-	
-	if event.is_action_pressed("ui_accept"):
-		do_a_jump.rpc_id(1)
+	super._unhandled_input(event)
 
 func _process(delta):
-	if multiplayer.get_unique_id() != pid:
-		return
-	var x_input = Input.get_axis("ui_left", "ui_right")
-	if x_input != last_x_input:
-		set_x_movement.rpc_id(1, x_input)
-		last_x_input = x_input
-	
-	var z_input = Input.get_axis("ui_up", "ui_down")
-	if z_input != last_z_input:
-		set_z_movement.rpc_id(1, z_input)
-		last_z_input = z_input
-
-func _apply_sync_state():
-	var s = buffered_sync.get_state(state_key)
-	if not s.valid and buffered_sync.got_initial_state:
-		push_warning("no valid sync state")
-		return
-	
-	position = s.start_state["p"].lerp(s.end_state["p"], s.progress)
-	rotation = s.start_state["r"].lerp(s.end_state["r"], s.progress)
+	super._process(delta)
 
 func _physics_process(delta):
-	if not multiplayer.is_server():
-		_apply_sync_state()
-		return
-		
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += gravity_vector * gravity * delta
-		
-	if jump_requested and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	jump_requested = false
-	
-	rotation.y = y_rotation
-	
-	var movement := Vector3(0, 0, 0)
-	if x_movement:
-		movement += Vector3(x_movement, 0, 0)
-	if z_movement:
-		movement += Vector3(0, 0, z_movement)
-	
-	if movement.length_squared() > 0:
-		movement = movement.rotated(Vector3(0, 1, 0), rotation.y)
-		movement = movement.normalized() * SPEED
-		
-	velocity.z = movement.z
-	velocity.x = movement.x
-
-	move_and_slide()
-	
-	buffered_sync.amend_server_state(state_key, {
-		"p": position,
-		"r": rotation
-	})
+	super._physics_process(delta)
