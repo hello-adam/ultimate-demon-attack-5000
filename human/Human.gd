ef78f9889f0915d891a4857dbdfe496bfca8039f
Adam Nash <adam@smr.llc>
2024-01-27 13:29:39 -0500

    refine player messaging and add dummy interaction for human

--- human/Human.gd
+++ human/Human.gd
@@ -1,9 +1,14 @@
 extends PlayerBase
 
+var popup_scn = preload("res://ui/PopupMessage.tscn")
+var popup_msg: PopupMessage
+
 func _ready():
 	super._ready()
 	if multiplayer.get_unique_id() == pid:
-		$InitPopup.popup_centered_ratio.call_deferred(0.5)
+		popup_msg = popup_scn.instantiate()
+		add_child(popup_msg)
+		popup_msg.show_message("You are a Human.\n\nYou must find your cat.\nPet cats to stay lucid.\n\nDon't pet weird cats.")
 
 func _input(event):
 	super._input(event)
@@ -13,6 +18,22 @@ func _unhandled_input(event):
 
 func _process(delta):
 	super._process(delta)
+	
+	if multiplayer.is_server():
+		if interact_requested:
+			dummy_interaction()
+			interact_requested = false
+
+func dummy_interaction():
+	if not multiplayer.is_server():
+		return
+	
+	print("dummy interaction start")
+	set_interacting.rpc(true)
+	await get_tree().create_timer(1.0).timeout
+	set_interacting.rpc(false)
+	print("dummy interaction done")
 
 func _physics_process(delta):
 	super._physics_process(delta)
+
