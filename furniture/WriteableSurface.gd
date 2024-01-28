9cb6efa4fe8d2bcb165e0733eaecd6141c63e50c
simonmnash <simon.m.nash@gmail.com>
2024-01-27 21:22:48 -0500

    randomize text angle

--- furniture/WriteableSurface.gd
+++ furniture/WriteableSurface.gd
@@ -12,9 +12,15 @@ var label_text:
 @onready var camera: Camera3D = $Camera3D
 @onready var light: Node3D = $Light
 
+
 func _ready():
 	light.visible = false
-	label_text = ""
+	label_text = "Test Text"
+	var max_angle_degrees = 30
+	var random_radians = randf() * deg_to_rad(max_angle_degrees)
+	if randi() % 2 == 0:
+		random_radians = random_radians * -1
+	$Label3D.rotate_object_local(Vector3(0, 0, 1), random_radians)
 
 func _process(delta):
 	pass
